e6f53b39a4a03d37837681b9c31b0fdf60314161
Korniliy M.D. <korniliyyablonskiy@gmail.com>
2025-12-30 20:08:31 +0200

    Add files via upload

---
+++ homework19/js/script.js
@@ -0,0 +1,21 @@
+"use strict";
+
+document.addEventListener('click', documentActions)
+
+function documentActions(e){
+  const targetElement = e.target;
+
+  if(targetElement.closest('.icon-menu')){
+    document.documentElement.toggleAttribute('data-menu-open');
+  }
+}
+
+const header = document.querySelector(".header");
+
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 0) {
+    header.classList.add("header--scroll-state");
+  } else {
+    header.classList.remove("header--scroll-state");
+  }
+});
